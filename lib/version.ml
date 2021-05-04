e8d90a832261fed49c6341012f56a7acba7c020a
TitiFix <github.titifix@gmail.com>
2021-05-04 20:29:30 +0200

    Merge PR 1144

--- lib/version.ml
+++ lib/version.ml
@@ -1,6 +1,6 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
-let txt = "7.0.0"
+let txt = "7.0.0.21050420_TitiFix_wsglo" (* from TitiFix_wsglo commit b02e1716 Apr_30_09:21:02_2021 *)
 
 let available_languages =
   ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
